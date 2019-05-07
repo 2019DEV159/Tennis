b42f755a8cf7ba0f0ebb709a8df87c73df2ea2b0
Etienne Jézéquel <>
2019-05-07 13:26:00 +0200

    Add Player model

---
+++ Tennis.playground/Sources/Player.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+class Player {
+    
+    // Score
+    // 0 => 0
+    // 1 => 15
+    // 2 => 30
+    // 3 => 40
+    private var score: Int = Constants.defaultStartingScore
+    private var name: String = ""
+    
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    // MARK: - Publics functions
+    
+    func scored() {
+    }
+    
+    func getScore() -> Int {
+        return 0
+    }
+    
+    func getName() -> String {
+        return ""
+    }
+}
+
