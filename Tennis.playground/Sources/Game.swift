bd64e7f28cae8034712c39c6fdbccfd49cbbf3ce
Etienne Jézéquel <>
2019-05-07 13:39:43 +0200

    Add Scored function

--- Tennis.playground/Sources/Game.swift
+++ Tennis.playground/Sources/Game.swift
@@ -2,13 +2,17 @@ import Foundation
 
 public class Game {
     
-    // MARK: - Privates functions
+    // MARK: - Privates
     
     private var player1: Player = Player(name: "Player 1")
     private var player2: Player = Player(name: "Player 2")
     private var gameIsEnded: Bool = false
     
     // MARK: - Privates functions
+    
+    fileprivate func playerScored(player: Player) {
+        player.scored()
+    }
     
     // MARK: - Publics functions
     
