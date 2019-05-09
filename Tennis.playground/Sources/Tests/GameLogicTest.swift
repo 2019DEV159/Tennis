30fed4ddb78514032164fe223d69a21c276464be
2019DEV159 <>
2019-05-09 09:11:24 +0200

    Add new class to manage Game logic

---
+++ Tennis.playground/Sources/Tests/GameLogicTest.swift
@@ -0,0 +1,15 @@
+import Foundation
+import XCTest
+
+public class GameLogicTest: XCTestCase {
+    
+    private var game = GameLogic()
+    
+    public override func setUp() {
+        // This method is called before the invocation of each test method in the class.
+    }
+    
+    public override func tearDown() {
+        // This method is called after the invocation of each test method in the class.
+    }
+}
